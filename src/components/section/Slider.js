bbd671f681c7aa7b0b327e640d6225a7369ee4db
piowm123 <piowm123@gmail.com>
2022-10-26 13:20:46 +0900

    slider

--- src/components/section/Slider.js
+++ src/components/section/Slider.js
@@ -7,70 +7,72 @@ import "swiper/css/pagination";
 
 function slider() {
   return (
-    <div className="slider__inner">
-      <Swiper
-        autoplay={{
-          delay: 3000,
-          desableOnInteraction: false,
-        }}
-        navigation={true}
-        pagination={{ clickable: true }}
-        modules={[Navigation, Pagination, Autoplay]}
-        className="mySwiper"
-      >
-        <SwiperSlide>
-          <div className="desc">
-            <span>The Fruit</span>
-            <h3>Sweet Fresh</h3>
-            <p>
-              싱싱한 과일이 왔어요~ 맛도 좋고 향도 좋고 몸에도 좋은 그 맛!
-              <br />
-              과일의 여러가지 쓰임새에 대해 알아봅시다~!
-            </p>
-            <div className="btn">
-              <a href="/">자세히 보기</a>
-              <a href="/" className="black">
-                사이트 보기
-              </a>
+    <section id="sliderType" className="slider__wrap nexon">
+      <div className="slider__inner">
+        <Swiper
+          autoplay={{
+            delay: 3000,
+            desableOnInteraction: false,
+          }}
+          navigation={true}
+          pagination={{ clickable: true }}
+          modules={[Navigation, Pagination, Autoplay]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <div className="desc">
+              <span>The Fruit</span>
+              <h3>Sweet Fresh</h3>
+              <p>
+                싱싱한 과일이 왔어요~ 맛도 좋고 향도 좋고 몸에도 좋은 그 맛!
+                <br />
+                과일의 여러가지 쓰임새에 대해 알아봅시다~!
+              </p>
+              <div className="btn">
+                <a href="/">자세히 보기</a>
+                <a href="/" className="black">
+                  사이트 보기
+                </a>
+              </div>
             </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="desc">
-            <span>The Fruit</span>
-            <h3>Sweet Fresh</h3>
-            <p>
-              싱싱한 과일이 왔어요~ 맛도 좋고 향도 좋고 몸에도 좋은 그 맛!
-              <br />
-              과일의 여러가지 쓰임새에 대해 알아봅시다~!
-            </p>
-            <div className="btn">
-              <a href="/">자세히 보기</a>
-              <a href="/" className="black">
-                사이트 보기
-              </a>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="desc">
+              <span>The Fruit</span>
+              <h3>Sweet Fresh</h3>
+              <p>
+                싱싱한 과일이 왔어요~ 맛도 좋고 향도 좋고 몸에도 좋은 그 맛!
+                <br />
+                과일의 여러가지 쓰임새에 대해 알아봅시다~!
+              </p>
+              <div className="btn">
+                <a href="/">자세히 보기</a>
+                <a href="/" className="black">
+                  사이트 보기
+                </a>
+              </div>
             </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="desc">
-            <span>The Fruit</span>
-            <h3>Sweet Fresh</h3>
-            <p>
-              싱싱한 과일이 왔어요~ 맛도 좋고 향도 좋고 몸에도 좋은 그 맛!
-              <br />
-              과일의 여러가지 쓰임새에 대해 알아봅시다~!
-            </p>
-            <div className="btn">
-              <a href="/">자세히 보기</a>
-              <a href="/" className="black">
-                사이트 보기
-              </a>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="desc">
+              <span>The Fruit</span>
+              <h3>Sweet Fresh</h3>
+              <p>
+                싱싱한 과일이 왔어요~ 맛도 좋고 향도 좋고 몸에도 좋은 그 맛!
+                <br />
+                과일의 여러가지 쓰임새에 대해 알아봅시다~!
+              </p>
+              <div className="btn">
+                <a href="/">자세히 보기</a>
+                <a href="/" className="black">
+                  사이트 보기
+                </a>
+              </div>
             </div>
-          </div>
-        </SwiperSlide>
-      </Swiper>
-    </div>
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </section>
   );
 }
 
